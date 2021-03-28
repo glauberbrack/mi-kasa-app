69a4ac2131dc37fc572143d9b3207e30ce41bf63
Glauber Brack <glauber@brack.com.br>
2021-03-27 22:56:38 -0300

    feat: create app theme

---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: '#594aff',
+  backgroundLight: '#2c285b',
+  backgroundDarl: '#1e1d42',
+  gray: '#e5e5e5',
+  white: '#ffffff',
+  purpleTransparent: 'rgba(45, 40, 91, .64)',
+  darkTransparent: 'rgba(18, 16, 37, .59)',
+};
