c7392d40bb33e5d337ea35bf859f73b2bec7d522
Glauber Brack <glauber@brack.com.br>
2021-03-27 22:37:25 -0300

    init project

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const App: () => Node = () => {
+  return <SafeAreaView />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
